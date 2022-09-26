d825ace9398beee077755668e2ddc571adfaa76c
tornorr <80989791+tornorr@users.noreply.github.com>
2022-09-25 19:28:09 -0600

    Update remote.py

--- remote.py
+++ remote.py
@@ -22,7 +22,7 @@ class MixedBag(commands.Cog):
 
     @commands.command()
     async def remote(self, ctx, channel, *, remotemsg):
-        channeldict = {"general":626180297993748499, "memes":626182840517918760, "dragonvale":626181797696503818, "mods":276384829593878529}
+        channeldict = {"general":626180297993748499, "memes":626182840517918760, "dragonvale":626181797696503818, "mods":276384829593878529, "test":904179778758774824}
         channel = ctx.get_channel(channeldict[channel])
         if ctx.guild == 720120185289310229:
             await ctx.send(f'{remotemsg}')
