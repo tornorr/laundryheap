dce3724830cc4c324b38c2653a3447885bb1ce2e
tornorr <80989791+tornorr@users.noreply.github.com>
2022-09-25 20:41:41 -0600

    Update remote.py

--- remote/remote.py
+++ remote/remote.py
@@ -6,10 +6,6 @@ from discord.ext import commands
 class Remote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        print(message.content)
 
     @commands.command()
     async def pet(self, ctx):
