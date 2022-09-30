ec03d89eb5fa6511b88e3c52073571ba12782db3
tornorr <80989791+tornorr@users.noreply.github.com>
2022-09-29 18:06:59 -0600

    Update remote.py

--- remote/remote.py
+++ remote/remote.py
@@ -8,21 +8,8 @@ class Remote(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def pet(self, ctx):
-        petMod = random.randint(1,100)
-        if petMod <= 79:
-            await ctx.send('YOOOOO! <:dv_pogberry:894740347698184232>')
-        elif petMod <= 94:
-            await ctx.send('....wha? huh? <:dv_:dv_drowsysnooz:894740491453743115>')
-        elif petMod <= 99:
-            await ctx.send('uh- uhh- az]jmr7c_w9irp <:dv_blizzardderp:894740491453743115>')
-        else:
-            await ctx.send("It's aurorin' time <:dv_trollrora:894740491453743115>")
-        
-
-    @commands.command()
     async def remote(self, ctx, channel, *, remotemsg):
-        channeldict = {"general":626180297993748499, "memes":626182840517918760, "dragonvale":626181797696503818, "mods":276384829593878529, "test":904179778758774824, "herald":946617550442213386}
+        channeldict = {"general":626180297993748499, "memes":626182840517918760, "dragonvale":626181797696503818, "mods":276384829593878529, "test":904179778758774824, "herald":946617550442213386, "oracle":818011940160405534}
         channel = self.bot.get_channel(channeldict[channel])
         if ctx.guild.id == 720120185289310229:
             await channel.send(remotemsg)
